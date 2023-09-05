d2af4e2a45650b28c26f06c75dbdd57550cc0629
CircleCI Runner CI <eng-runner@circleci.com>
2023-09-05 11:07:32 +0000

    Release 3.0.12 circleci-runner

--- Casks/circleci-runner.rb
+++ Casks/circleci-runner.rb
@@ -1,11 +1,11 @@
 cask "circleci-runner" do
-  version "3.0.11"
+  version "3.0.12"
   name "circleci-runner"
   desc "The self-hosted runner agent for CircleCI"
   homepage "https://circleci.com/docs/2.0/runner-overview/"
 
-  intelSHA = "144abf3e347ec47c3c87aa0122a50a9e07abc7056bd6d152c9e309c69b1e92ef"
-  armSHA = "97ac9103946aaad1897528e8ceffe484d7092e3aedf55026d3f2d29413e185a7"
+  intelSHA = "a3c85153d8b15f39e5e481e86f4d8ddcf60055188f8e37981782b55fb1f95fba"
+  armSHA = "37d4b198aa95feadc27a23ef1d01c61de610d04917bcaf0044a7b5bf1fe39de7"
   
   if Hardware::CPU.intel? 
     sha256 "#{intelSHA}"
